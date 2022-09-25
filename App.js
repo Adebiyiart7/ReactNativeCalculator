7ac596a1f0ffc9bfb0f3e6520afe3fd49c5b7c47
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-09-26 00:07:30 +0100

    Creating a new project ReactNativeCalculator

--- App.js
+++ App.js
@@ -1,78 +1,20 @@
-import React, { useState } from "react";
-import {
-  Alert,
-  FlatList,
-  View,
-  Platform,
-  StatusBar,
-  StyleSheet,
-} from "react-native";
-import AddItem from "./components/AddItem";
-import Header from "./components/Header";
-import ListItem from "./components/ListItem";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const App = () => {
-  const [items, setItems] = useState([
-    {
-      id: Math.random().toString().slice(2, 8),
-      text: "Milk",
-    },
-    {
-      id: Math.random().toString().slice(2, 8),
-      text: "Rice",
-    },
-    {
-      id: Math.random().toString().slice(2, 8),
-      text: "Bread",
-    },
-    {
-      id: Math.random().toString().slice(2, 8),
-      text: "Sugar",
-    },
-    {
-      id: Math.random().toString().slice(2, 8),
-      text: "Floor",
-    },
-  ]);
-
-  const onDelete = (id) => {
-    setItems(items.filter((item) => item.id !== id));
-  };
-
-  const onAddItem = (item) => {
-    // if (!item) {
-    //   // Alert.alert("Error", "Please enter an item", { text: "OK" });
-    // } else {
-    const newItem = {
-      id: "2",
-      text: item,
-    };
-
-    setItems((prevItems) => [newItem, ...prevItems]);
-    // }
-  };
-
   return (
     <View style={styles.container}>
-      <Header title={"Shopping List"} />
-      <AddItem onAddItem={onAddItem} />
-      <FlatList
-        data={items}
-        renderItem={({ item, index }) => (
-          <ListItem item={item} index={index} onDelete={onDelete} />
-        )}
-      />
+      <Text>Hello World!!!</Text>
     </View>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
-    paddingTop:
-      Platform.OS !== "web" &&
-      (Platform.OS === "android" ? StatusBar.currentHeight : 20),
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
-
-export default App;
